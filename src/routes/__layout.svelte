4560bd676d8eec28755ba7b7591a14acbb18027c
jAcee12 <jaydoncameron80@protonmail.com>
2022-02-19 17:20:26 +1100

    .

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -6,24 +6,7 @@
 	import '../app.scss';
 	import Header from '$components/header/Header.svelte';
 	import Footer from '$lib/components/footer/Footer.svelte';
-	import { onMount } from 'svelte';
-	import { isMobile } from '../Stores';
-
-	/* Patch for SvelteKit/Sapper treating anchor tags as route changes 
-	https://github.com/sveltejs/sapper/issues/904#issuecomment-532975259 */
-	// onMount(() => {
-	// 	document.querySelectorAll('a').forEach((a) => {
-	// 		if (!a.hash || !document.querySelectorAll(`a[href=${a.hash}]`).length) return;
-	// 		a.href = window.location.origin + window.location.pathname + a.hash;
-	// 	});
-
-		
-	// });
 </script>
-
-<!-- <svelte:head>
-	<link rel="stylesheet" href="node_modules/modern-normalize/modern-normalize.css" />
-</svelte:head> -->
 
 <div class="backdrop">
 	<Header />
